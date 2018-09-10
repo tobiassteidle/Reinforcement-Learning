4cf8ef25416816aaf30913b31b911ceb4fae0b1f
Tobias Steidle <tobias.steidle@softwaredev.de>
2018-09-10 16:04:58 +0200

    Update README

--- OpenAI/Taxi-v2/main.py
+++ OpenAI/Taxi-v2/main.py
@@ -5,4 +5,4 @@ import numpy as np
 
 env = gym.make('Taxi-v2')
 agent = Agent()
-avg_rewards, best_avg_reward = interact(env, agent, 40000)
+avg_rewards, best_avg_reward = interact(env, agent)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Reinforcement-Learning
+
+### Udacity - Deep Reinforcement Learning Nanodegree Program
+
+[Taxi-v2](OpenAI/)
