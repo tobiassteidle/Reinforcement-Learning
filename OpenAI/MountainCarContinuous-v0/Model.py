14488c17dc63f7b1b821f3b5a56955103cabe667
Tobias Steidle <tobias.steidle@softwaredev.de>
2018-10-17 07:10:15 +0200

    fix: Remove unused code.

--- OpenAI/MountainCarContinuous-v0/Model.py
+++ OpenAI/MountainCarContinuous-v0/Model.py
@@ -69,9 +69,7 @@ class Critic(nn.Module):
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.bn1 = nn.BatchNorm1d(fc1_units)
-        #self.ln1 = nn.LayerNorm(fc1_units)
         self.fc2 = nn.Linear(fc1_units+action_size, fc2_units)
-        #self.ln2 = nn.LayerNorm(fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
         self.reset_parameters()
 
@@ -84,11 +82,8 @@ class Critic(nn.Module):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
         xs = self.fc1(state)
         xs = self.bn1(xs)
-        #xs = self.ln1(xs)
         xs = F.leaky_relu(xs)
         x = torch.cat((xs, action), dim=1)
-
         x = self.fc2(x)
-        #x = self.ln2(x)
         x = F.leaky_relu(x)
         return self.fc3(x)
