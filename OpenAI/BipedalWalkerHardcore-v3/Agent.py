ea49e2d1692fe57a7138057b4e2f963fe9fa1b80
Tobias Steidle <tobias.steidle@softwaredev.de>
2020-11-03 16:41:14 +0100

    feat: First Version of BipedalWalkerHardcore-v3

---
+++ OpenAI/BipedalWalkerHardcore-v3/Agent.py
@@ -0,0 +1,175 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+# Hyperparameters
+gamma = 0.99                # discount for future rewards
+batch_size = 1024            # num of transitions sampled from replay buffer
+polyak = 0.995              # target policy update parameter (1-tau)
+policy_noise = 0.2          # target policy smoothing noise
+noise_clip = 0.5
+
+policy_delay = 2            # delayed policy updates parameter
+LR_ACTOR = 5e-4
+LR_CRITIC = 5e-4 # 0.0005
+
+WEIGHT_DECAY = 0.0
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, max_action):
+        super(Actor, self).__init__()
+
+        self.l1 = nn.Linear(state_dim, 400)
+        self.l2 = nn.Linear(400, 300)
+        self.l3 = nn.Linear(300, action_dim)
+
+        self.max_action = max_action
+
+    def forward(self, state):
+        a = F.relu(self.l1(state))
+        a = F.relu(self.l2(a))
+        a = torch.tanh(self.l3(a)) * self.max_action
+        return a
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+
+        self.l1 = nn.Linear(state_dim + action_dim, 400)
+        self.l2 = nn.Linear(400, 300)
+        self.l3 = nn.Linear(300, 1)
+
+    def forward(self, state, action):
+        state_action = torch.cat([state, action], 1)
+
+        q = F.relu(self.l1(state_action))
+        q = F.relu(self.l2(q))
+        q = self.l3(q)
+        return q
+
+class TD3:
+    def __init__(self, state_dim, action_dim, max_action):
+
+        self.actor = Actor(state_dim, action_dim, max_action).to(device)
+        self.actor_target = Actor(state_dim, action_dim, max_action).to(device)
+        self.actor_target.load_state_dict(self.actor.state_dict())
+        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=LR_ACTOR)
+
+        self.critic_1 = Critic(state_dim, action_dim).to(device)
+        self.critic_1_target = Critic(state_dim, action_dim).to(device)
+        self.critic_1_target.load_state_dict(self.critic_1.state_dict())
+        self.critic_1_optimizer = optim.Adam(self.critic_1.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
+
+        self.critic_2 = Critic(state_dim, action_dim).to(device)
+        self.critic_2_target = Critic(state_dim, action_dim).to(device)
+        self.critic_2_target.load_state_dict(self.critic_2.state_dict())
+        self.critic_2_optimizer = optim.Adam(self.critic_2.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
+
+        self.max_action = max_action
+
+    def select_action(self, state):
+        state = torch.FloatTensor(state.reshape(1, -1)).to(device)
+        return self.actor(state).cpu().data.numpy().flatten()
+
+    def mse(self, expected, targets, is_weights):
+        td_error = expected - targets
+        weighted_squared_error = is_weights * td_error * td_error
+        return torch.sum(weighted_squared_error) / torch.numel(weighted_squared_error)
+
+    def update(self, replay_buffer, learn_batch_size=100):
+
+        for i in range(learn_batch_size):
+            # Sample a batch of transitions from replay buffer:
+            idxs, experiences, is_weights = replay_buffer.sample(batch_size)
+
+            state = torch.from_numpy(np.vstack([e[0] for e in experiences if e is not None])).float().to(device)
+            action = torch.from_numpy(np.vstack([e[1] for e in experiences if e is not None])).float().to(device)
+            reward = torch.from_numpy(np.vstack([e[2] for e in experiences if e is not None])).float().to(device)
+            next_state = torch.from_numpy(np.vstack([e[3] for e in experiences if e is not None])).float().to(device)
+            done = torch.from_numpy(np.vstack([e[4] for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+
+            is_weights =  torch.from_numpy(is_weights).float().to(device)
+
+            # Select next action according to target policy:
+            noise = torch.empty_like(action).data.normal_(0, policy_noise).to(device)
+            noise = noise.clamp(-noise_clip, noise_clip)
+            next_action = (self.actor_target(next_state) + noise)
+            next_action = next_action.clamp(-self.max_action, self.max_action)
+
+            # Compute target Q-value:
+            target_Q1 = self.critic_1_target(next_state, next_action)
+            target_Q2 = self.critic_2_target(next_state, next_action)
+            target_Q = torch.min(target_Q1, target_Q2)
+            target_Q = reward + ((1-done) * gamma * target_Q).detach()
+
+            # Optimize Critic 1:
+            current_Q1 = self.critic_1(state, action)
+            errors1 = np.abs((current_Q1 - target_Q).detach().cpu().numpy())
+            #loss_Q1 = self.mse(current_Q1, target_Q, is_weights)
+            loss_Q1 = F.mse_loss(current_Q1, target_Q)
+
+
+            self.critic_1_optimizer.zero_grad()
+            loss_Q1.backward()
+            self.critic_1_optimizer.step()
+
+            # Update priorities in the replay buffer
+            replay_buffer.batch_update(idxs, errors1)
+
+            # Optimize Critic 2:
+            current_Q2 = self.critic_2(state, action)
+            #loss_Q2 = self.mse(current_Q2, target_Q, is_weights)
+            loss_Q2 = F.mse_loss(current_Q2, target_Q)
+            self.critic_2_optimizer.zero_grad()
+            loss_Q2.backward()
+            self.critic_2_optimizer.step()
+
+            # Delayed policy updates:
+            if i % policy_delay == 0:
+                # Compute actor loss:
+                actor_loss = -self.critic_1(state, self.actor(state)).mean()
+
+                # Optimize the actor
+                self.actor_optimizer.zero_grad()
+                actor_loss.backward()
+                self.actor_optimizer.step()
+
+                # Polyak averaging update:
+                for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
+                    target_param.data.copy_( (polyak * target_param.data) + ((1-polyak) * param.data))
+
+                for param, target_param in zip(self.critic_1.parameters(), self.critic_1_target.parameters()):
+                    target_param.data.copy_( (polyak * target_param.data) + ((1-polyak) * param.data))
+
+                for param, target_param in zip(self.critic_2.parameters(), self.critic_2_target.parameters()):
+                    target_param.data.copy_( (polyak * target_param.data) + ((1-polyak) * param.data))
+
+
+    def save(self, directory, name):
+        torch.save(self.actor.state_dict(), '%s/%s_actor.pth' % (directory, name))
+        torch.save(self.actor_target.state_dict(), '%s/%s_actor_target.pth' % (directory, name))
+
+        torch.save(self.critic_1.state_dict(), '%s/%s_crtic_1.pth' % (directory, name))
+        torch.save(self.critic_1_target.state_dict(), '%s/%s_critic_1_target.pth' % (directory, name))
+
+        torch.save(self.critic_2.state_dict(), '%s/%s_crtic_2.pth' % (directory, name))
+        torch.save(self.critic_2_target.state_dict(), '%s/%s_critic_2_target.pth' % (directory, name))
+
+    def load(self, directory, name):
+        self.actor.load_state_dict(torch.load('%s/%s_actor.pth' % (directory, name), map_location=lambda storage, loc: storage))
+        self.actor_target.load_state_dict(torch.load('%s/%s_actor_target.pth' % (directory, name), map_location=lambda storage, loc: storage))
+
+        self.critic_1.load_state_dict(torch.load('%s/%s_crtic_1.pth' % (directory, name), map_location=lambda storage, loc: storage))
+        self.critic_1_target.load_state_dict(torch.load('%s/%s_critic_1_target.pth' % (directory, name), map_location=lambda storage, loc: storage))
+
+        self.critic_2.load_state_dict(torch.load('%s/%s_crtic_2.pth' % (directory, name), map_location=lambda storage, loc: storage))
+        self.critic_2_target.load_state_dict(torch.load('%s/%s_critic_2_target.pth' % (directory, name), map_location=lambda storage, loc: storage))
+
+
+    def load_actor(self, directory, name):
+        self.actor.load_state_dict(torch.load('%s/%s_actor.pth' % (directory, name), map_location=lambda storage, loc: storage))
+        self.actor_target.load_state_dict(torch.load('%s/%s_actor_target.pth' % (directory, name), map_location=lambda storage, loc: storage))
